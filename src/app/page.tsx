cd17020b5aaa26fd9a1531cd8867259050f74066
Khairul <khairulihsannudin@mail.ugm.ac.id>
2024-02-25 09:03:03 +0700

    test prettier with husky

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -70,10 +70,10 @@ export default function Home() {
           >
             <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
           </svg>
-          <input required type="text"  className="grow" placeholder="Name" />
+          <input required type="text" className="grow" placeholder="Name" />
         </label>
         <textarea
-        required
+  required
           className="textarea textarea-primary textarea-bordered textarea-md w-full max-w-md"
           placeholder="Message for me"
         ></textarea>
